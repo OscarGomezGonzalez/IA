560295c17265af744c3d5610dd50b8a41ff863c1
Oscar Gomez Gonzalez <oscar.gomezgonzalez95@gmail.com>
2019-10-24 11:50:59 +0200

    2 y 3
    Planteamiento (inicio y estructura)

--- Homework_2/main.m
+++ Homework_2/main.m
@@ -9,3 +9,31 @@ X=data(:,1:2);
 y=data(:,3);
 
 plotData(X,y);
+
+
+%% Apartado 2
+
+
+%gráfica de convergencia de descenso del gradiente
+
+%imprimir la tasa de acierto y la grafica con la frontera de decisión
+plotDecisionBoundary();
+
+
+%% Apartado 3
+%alfa, número de iteraciones y tasa de acierto
+
+%crear mas atributos atraves de mapFeature
+mapFeature(); %devuelve una matriz de 28 atributos
+
+%necesitamos que el gradiente converja
+%gráfica de convergencia de descenso del gradiente
+
+%necesitamos que la tasa de acierto sea mayor o igual que 82%
+%imprimir la tasa de acierto y la grafica con la frontera de decisión
+plotDecisionBoundary();
+
+
+
+
+
